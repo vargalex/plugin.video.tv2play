44a26ad74e97767b267c476e12f885fd57e2521c
aky-01 <65510015+aky-01@users.noreply.github.com>
2021-03-14 12:49:02 +0100

    Speciális karakter dekódolás javítása

--- resources/lib/utils.py
+++ resources/lib/utils.py
@@ -10,3 +10,12 @@ def py2_encode(s, encoding='utf-8', errors='strict'):
     if sys.version_info[0] == 2 and isinstance(s, unicode):
         s = s.encode(encoding, errors)
     return s
+
+def py2_decode(s, encoding='utf-8', errors='strict'):
+    """
+    Decode Python 2 ``str`` to ``unicode``
+    In Python 3 the string is not changed.
+    """
+    if sys.version_info[0] == 2 and isinstance(s, str):
+        s = s.decode(encoding, errors)
+    return s
