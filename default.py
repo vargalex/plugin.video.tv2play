360c678416299000568e80fe654bd837a82f47ef
Gabor Varga <vargagab@gmail.com>
2024-05-02 19:14:45 +0200

    Random magyarországi IP cím használatának lehetősége

--- default.py
+++ default.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import xbmc, xbmcgui, xbmcaddon, re, xbmcplugin, json, os, sys, base64, time
+import xbmc, xbmcgui, xbmcaddon, re, xbmcplugin, json, os, sys, base64, time, locale, random, struct, socket
 from resources.lib import client, control
 from resources.lib.utils import py2_decode
 
@@ -17,11 +17,265 @@ api_url = "%s/api" % base_url
 auth_url = "%s/authenticate" % api_url
 userinfo_url = "%s/users/me" % api_url
 logout_url = "%s/logout" % api_url
+try:
+    locale.setlocale(locale.LC_ALL, "")
+except:
+    pass
+
+def produce_header():
+    hungarianIPRanges = [
+        "5.38.128.0/17",
+        "5.63.192.0/18",
+        "5.148.192.0/18",
+        "5.187.128.0/17",
+        "5.204.0.0/16",
+        "5.206.128.0/18",
+        "31.46.0.0/16",
+        "31.171.224.0/20",
+        "37.17.160.0/20",
+        "37.76.0.0/17",
+        "37.191.0.0/18",
+        "37.220.128.0/20",
+        "37.220.192.0/18",
+        "37.234.0.0/16",
+        "46.35.192.0/19",
+        "46.107.0.0/16",
+        "46.139.0.0/16",
+        "46.249.128.0/19",
+        "46.251.16.0/20",
+        "62.77.208.0/20",
+        "62.77.224.0/20",
+        "62.100.224.0/19",
+        "62.112.192.0/19",
+        "62.165.192.0/18",
+        "62.201.64.0/18",
+        "77.110.128.0/19",
+        "77.110.160.0/19",
+        "77.111.64.0/19",
+        "77.111.96.0/20",
+        "77.111.112.0/20",
+        "77.111.128.0/18",
+        "77.221.32.0/19",
+        "77.234.64.0/19",
+        "77.242.144.0/20",
+        "77.243.208.0/20",
+        "78.92.0.0/16",
+        "78.108.16.0/20",
+        "78.131.0.0/17",
+        "78.139.0.0/18",
+        "78.153.96.0/19",
+        "79.120.128.0/19",
+        "79.120.176.0/20",
+        "79.120.192.0/19",
+        "79.120.224.0/20",
+        "79.121.0.0/18",
+        "79.121.64.0/19",
+        "79.122.0.0/17",
+        "79.172.192.0/18",
+        "80.64.64.0/20",
+        "80.77.112.0/20",
+        "80.95.64.0/20",
+        "80.95.80.0/20",
+        "80.98.0.0/15",
+        "80.244.96.0/20",
+        "80.249.160.0/20",
+        "80.252.48.0/20",
+        "81.0.64.0/20",
+        "81.16.192.0/20",
+        "81.17.176.0/20",
+        "81.22.176.0/20",
+        "81.93.192.0/20",
+        "81.94.176.0/20",
+        "81.94.240.0/20",
+        "81.160.128.0/17",
+        "81.182.0.0/15",
+        "82.131.128.0/19",
+        "82.131.160.0/20",
+        "82.131.224.0/19",
+        "82.141.128.0/18",
+        "82.150.32.0/19",
+        "84.0.0.0/14",
+        "84.21.0.0/19",
+        "84.206.0.0/16",
+        "84.224.0.0/16",
+        "84.225.0.0/17",
+        "84.225.128.0/18",
+        "84.225.192.0/18",
+        "84.236.0.0/17",
+        "85.66.0.0/15",
+        "85.90.160.0/19",
+        "85.238.64.0/19",
+        "86.59.128.0/17",
+        "86.101.0.0/16",
+        "86.109.64.0/19",
+        "87.97.0.0/18",
+        "87.97.64.0/20",
+        "87.97.80.0/20",
+        "87.97.96.0/19",
+        "87.101.112.0/20",
+        "87.229.0.0/17",
+        "87.242.0.0/18",
+        "88.87.224.0/20",
+        "88.132.0.0/17",
+        "88.132.128.0/18",
+        "88.132.192.0/19",
+        "88.132.224.0/20",
+        "88.209.192.0/18",
+        "89.132.0.0/14",
+        "89.147.64.0/20",
+        "89.148.64.0/18",
+        "89.223.128.0/17",
+        "89.251.32.0/20",
+        "91.82.96.0/19",
+        "91.82.192.0/20",
+        "91.83.0.0/19",
+        "91.83.64.0/20",
+        "91.83.160.0/20",
+        "91.83.192.0/20",
+        "91.83.208.0/20",
+        "91.83.224.0/20",
+        "91.104.0.0/16",
+        "91.120.0.0/16",
+        "91.135.112.0/20",
+        "91.137.128.0/17",
+        "91.144.64.0/18",
+        "91.146.128.0/18",
+        "91.147.192.0/19",
+        "92.52.192.0/19",
+        "92.52.224.0/19",
+        "92.61.96.0/20",
+        "92.61.112.0/20",
+        "92.63.240.0/20",
+        "92.245.64.0/19",
+        "92.249.128.0/17",
+        "93.89.160.0/20",
+        "94.21.0.0/16",
+        "94.27.128.0/17",
+        "94.44.0.0/16",
+        "94.248.128.0/19",
+        "94.248.160.0/20",
+        "94.248.192.0/19",
+        "94.248.240.0/20",
+        "95.168.32.0/19",
+        "95.168.64.0/19",
+        "95.171.64.0/19",
+        "109.61.0.0/18",
+        "109.61.64.0/20",
+        "109.61.112.0/20",
+        "109.74.48.0/20",
+        "109.105.0.0/19",
+        "109.110.128.0/19",
+        "109.199.32.0/19",
+        "130.43.192.0/18",
+        "130.93.192.0/18",
+        "134.255.0.0/17",
+        "145.236.0.0/16",
+        "146.110.0.0/16",
+        "147.7.0.0/16",
+        "148.6.0.0/16",
+        "149.200.0.0/17",
+        "151.0.64.0/18",
+        "152.66.0.0/16",
+        "157.181.0.0/16",
+        "158.249.0.0/16",
+        "160.114.0.0/16",
+        "171.19.0.0/16",
+        "171.31.0.0/16",
+        "176.63.0.0/16",
+        "176.77.128.0/17",
+        "176.226.0.0/17",
+        "176.241.0.0/18",
+        "178.48.0.0/16",
+        "178.164.128.0/17",
+        "178.210.224.0/19",
+        "178.238.208.0/20",
+        "188.6.0.0/16",
+        "188.36.0.0/16",
+        "188.44.128.0/17",
+        "188.127.128.0/19",
+        "188.142.128.0/19",
+        "188.142.160.0/19",
+        "188.142.192.0/18",
+        "188.143.0.0/17",
+        "188.156.0.0/15",
+        "193.6.0.0/16",
+        "193.68.32.0/19",
+        "193.91.64.0/19",
+        "193.224.0.0/16",
+        "193.225.0.0/16",
+        "194.38.96.0/19",
+        "194.88.32.0/19",
+        "194.143.224.0/19",
+        "194.149.0.0/19",
+        "194.149.32.0/19",
+        "194.152.128.0/19",
+        "194.176.224.0/19",
+        "195.38.96.0/19",
+        "195.56.0.0/16",
+        "195.70.32.0/19",
+        "195.111.0.0/16",
+        "195.184.0.0/19",
+        "195.184.160.0/19",
+        "195.199.0.0/16",
+        "195.228.0.0/16",
+        "212.16.128.0/19",
+        "212.24.160.0/19",
+        "212.40.64.0/19",
+        "212.40.96.0/19",
+        "212.48.240.0/20",
+        "212.51.64.0/18",
+        "212.52.160.0/19",
+        "212.92.0.0/19",
+        "212.96.32.0/19",
+        "212.105.224.0/19",
+        "212.108.192.0/18",
+        "213.16.64.0/18",
+        "213.157.96.0/19",
+        "213.163.0.0/19",
+        "213.163.32.0/19",
+        "213.178.96.0/19",
+        "213.181.192.0/19",
+        "213.197.80.0/20",
+        "213.197.96.0/19",
+        "213.222.128.0/18",
+        "213.253.192.0/18",
+        "217.13.32.0/20",
+        "217.13.96.0/20",
+        "217.20.128.0/20",
+        "217.21.16.0/20",
+        "217.27.208.0/20",
+        "217.65.96.0/20",
+        "217.65.112.0/20",
+        "217.79.128.0/20",
+        "217.112.128.0/20",
+        "217.116.32.0/20",
+        "217.144.48.0/20",
+        "217.150.128.0/20",
+        "217.173.32.0/20",
+        "217.197.176.0/20",
+    ]
+    headers = None
+    if control.setting("chooseRandomIP") == "true":
+        randomIP = control.setting("randomIP")
+        if randomIP == "":
+            xbmc.log("TV2Play: Random IP requested and not setted yet. Choosing one IP from Hungarian addresses.", xbmc.LOGINFO)
+            randomRange = hungarianIPRanges[random.randrange(len(hungarianIPRanges))]
+            xbmc.log("TV2Play: IP Range selected: %s" % randomRange, xbmc.LOGINFO)
+            addr, preflen = randomRange.split('/')
+            addr_min = struct.unpack('!L', socket.inet_aton(addr))[0]
+            addr_max = addr_min | (0xffffffff >> int(preflen))
+            randomIP = str(socket.inet_ntoa(struct.pack('!L', random.randint(addr_min, addr_max))))
+            control.setSetting("randomIP", randomIP)
+            xbmc.log("TV2Play: Using random IP for requests: %s" % randomIP, xbmc.LOGINFO)
+        headers = {"X-Forwarded-For": randomIP}
+    return headers
+
+headers = produce_header()
 
 def main_folders():
     artPath = py2_decode(control.artPath())
     addDirectoryItem("Műsorok", "musorok", os.path.join(artPath, "tv2play.png"), None)
-    r = client.request("%s/channels" % api_url)
+    r = client.request("%s/channels" % api_url, headers=headers)
     channels = sorted(json.loads(r), key=lambda k:k["id"])
     for channel in channels:
         if channel["slug"] != "spiler2" and (not channel["isPremium"] or hasPremium):
@@ -41,7 +295,7 @@ def musorok():
     allItemCnt = -1
     allItems = []
     while len(allItems) != allItemCnt:
-        r = client.request("https://tv2-bud.gravityrd-services.com/grrec-tv2-war/JSServlet4?rd=0,TV2_W_CONTENT_LISTING,800,[*platform:web;*domain:tv2play;*currentContent:SHOW;*country:HU;*userAge:16;*pagingOffset:%d],[displayType;channel;title;itemId;duration;isExtra;ageLimit;showId;genre;availableFrom;director;isExclusive;lead;url;contentType;seriesTitle;availableUntil;showSlug;videoType;series;availableEpisode;imageUrl;totalEpisode;category;playerId;currentSeasonNumber;currentEpisodeNumber;part;isPremium]" % pageOffset)
+        r = client.request("https://tv2-bud.gravityrd-services.com/grrec-tv2-war/JSServlet4?rd=0,TV2_W_CONTENT_LISTING,800,[*platform:web;*domain:tv2play;*currentContent:SHOW;*country:HU;*userAge:16;*pagingOffset:%d],[displayType;channel;title;itemId;duration;isExtra;ageLimit;showId;genre;availableFrom;director;isExclusive;lead;url;contentType;seriesTitle;availableUntil;showSlug;videoType;series;availableEpisode;imageUrl;totalEpisode;category;playerId;currentSeasonNumber;currentEpisodeNumber;part;isPremium]" % pageOffset, headers=headers)
         matches=re.search(r'(.*)var data = (.*)};(.*)', r, re.S)
         if matches:
             result = json.loads("%s}" % matches.group(2))
@@ -56,7 +310,10 @@ def musorok():
             allItemCnt = 0
             allItems = []
         pageOffset=len(allItems)
-    allItemsSorted=sorted(allItems, key=lambda k:k["title"])
+    if control.setting('programorder') == '1':
+        allItemsSorted=sorted(allItems, key=lambda k: locale.strxfrm(k["title"].lower()))
+    else:
+        allItemsSorted = allItems
     for item in allItemsSorted:
         if hasPremium or not "isPremium" in item or item["isPremium"] == "false":
             addDirectoryItem(item["title"].encode("utf-8"),
@@ -68,7 +325,7 @@ def musorok():
 
 
 def apiSearchSeason(season):
-    r = client.request("%s%s/search/%s" % (api_url, "/premium" if ispremium else "", param), cookie="jwt=%s" % jwtToken if jwtToken else None)
+    r = client.request("%s%s/search/%s" % (api_url, "/premium" if ispremium else "", param), cookie="jwt=%s" % jwtToken if jwtToken else None, headers=headers)
     data = json.loads(r)
     ribbons = []
     index = 0
@@ -91,7 +348,7 @@ def apiSearchSeason(season):
                 if thumb == '' and "imageUrl" in tab["showData"]:
                     thumb = "%s/%s" % (base_url, tab["showData"]["imageUrl"]) if "https://" not in tab["showData"]["imageUrl"] else tab["showData"]["imageUrl"]
     for ribbon in ribbons:
-        r = client.request("%s/ribbons/%s" % (api_url, ribbon), cookie="jwt=%s" % jwtToken if jwtToken else None)
+        r = client.request("%s/ribbons/%s" % (api_url, ribbon), cookie="jwt=%s" % jwtToken if jwtToken else None, headers=headers)
         if r:
             data = json.loads(r)
             addDirectoryItem(data["title"].encode("utf-8"),
@@ -102,7 +359,7 @@ def apiSearchSeason(season):
     endDirectory(type="tvshows")
 
 def apiSearch():
-    r = client.request("%s%s/search/%s" % (api_url, "/premium" if ispremium else "", param), cookie="jwt=%s" % jwtToken if jwtToken else None)
+    r = client.request("%s%s/search/%s" % (api_url, "/premium" if ispremium else "", param), cookie="jwt=%s" % jwtToken if jwtToken else None, headers=headers)
     data = json.loads(r)
     if "seasonNumbers" in data and len(data["seasonNumbers"])>0:
         if "seo" in data and "description" in data["seo"] and data["seo"]["description"] != None:
@@ -121,7 +378,7 @@ def apiSearch():
 
 
 def apiRibbons():
-    r = client.request("%s/ribbons/%s/%s" % (api_url, param, page), cookie="jwt=%s" % jwtToken if jwtToken else None)
+    r = client.request("%s/ribbons/%s/%s" % (api_url, param, page), cookie="jwt=%s" % jwtToken if jwtToken else None, headers=headers)
     data = json.loads(r)
     dirType = 'videos'
     for card in data["cards"]:
@@ -131,7 +388,7 @@ def apiRibbons():
             plot = ""
             if control.setting('fillLead') == 'true':
                 try:
-                    r = client.request("%s%s/search/%s" % (api_url, "/premium" if card["isPremium"] else "", card["slug"]), cookie="jwt=%s" % jwtToken if jwtToken else None)
+                    r = client.request("%s%s/search/%s" % (api_url, "/premium" if card["isPremium"] else "", card["slug"]), cookie="jwt=%s" % jwtToken if jwtToken else None, headers=headers)
                     episode = json.loads(r)
                     plot = episode["lead"] if "lead" in episode else ""
                     if plot.startswith("<p>"):
@@ -156,7 +413,7 @@ def apiRibbons():
                                 thumb, 
                                 "DefaultFolder.png", 
                                 meta={'title': title, 'plot': card["lead"].encode('utf-8') if "lead" in card else ''})           
-    r = client.request("%s/ribbons/%s/%d" % (api_url, param, int(page)+1))
+    r = client.request("%s/ribbons/%s/%d" % (api_url, param, int(page)+1), cookie="jwt=%s" % jwtToken if jwtToken else None, headers=headers)
     if r != None:
         addDirectoryItem(u'[I]K\u00F6vetkez\u0151 oldal >>[/I]', 'apiribbons&param=%s&page=%d' % (param, int(page)+1), '', 'DefaultFolder.png')
     endDirectory(type=dirType)
@@ -167,22 +424,22 @@ def playVideo():
         splittedParam = param.split("/")
         splittedParam[-1] = quote_plus(splittedParam[-1])
         joinedParam = "/".join(splittedParam)
-        r = client.request("%s%s/search/%s" % (api_url, "/premium" if ispremium else "", joinedParam), cookie="jwt=%s" % jwtToken if jwtToken else None)
+        r = client.request("%s%s/search/%s" % (api_url, "/premium" if ispremium else "", joinedParam), cookie="jwt=%s" % jwtToken if jwtToken else None, headers=headers)
         data = json.loads(r)
         playerId = data["playerId"]
         title = data["title"]
         plot = data["lead"] if "lead" in data else ""
         thumb = "%s/%s" % (base_url, data["imageUrl"]) if "https://" not in data["imageUrl"] else data["imageUrl"]
-        r = client.request("%s%s/streaming-url?playerId=%s&stream=undefined" % (api_url, "/premium" if ispremium else "", playerId), cookie="jwt=%s" % jwtToken if jwtToken else None)
+        r = client.request("%s%s/streaming-url?playerId=%s&stream=undefined" % (api_url, "/premium" if ispremium else "", playerId), cookie="jwt=%s" % jwtToken if jwtToken else None, headers=headers)
         data = json.loads(r)
         if (data["geoBlocked"] != False):
             xbmcgui.Dialog().notification("TV2 Play", "A tartalom a tartózkodási helyedről sajnos nem elérhető!", xbmcgui.NOTIFICATION_ERROR)
             return
-        r = client.request(data["url"])
+        r = client.request(data["url"], headers=headers)
         json_data = json.loads(r)
         m3u_url = json_data['bitrates']['hls']
         m3u_url = json_url = re.sub('^//', 'https://', m3u_url)
-        r = client.request(m3u_url)
+        r = client.request(m3u_url, headers=headers)
 
         root = os.path.dirname(m3u_url)
         sources = m3u8_parser.parse(r)
@@ -260,7 +517,7 @@ def doLogout():
 def logout():
     if control.yesnoDialog("Valóban ki szeretne jelentkezni?") == 1:
         try:
-            res = client.request(logout_url, cookie="jwt=%s" % jwtToken if jwtToken else None)
+            res = client.request(logout_url, cookie="jwt=%s" % jwtToken if jwtToken else None, headers=headers)
         except:
             pass
         doLogout()
@@ -269,10 +526,13 @@ def login():
     loginData = json.dumps({"email": control.setting("email"), "password": control.setting("password"), "stayLoggedIn": True, "deviceToken": {"token": None, "platform": "web"}}).encode('utf-8')
     jwtToken = ""
     try:
-        authAnswer = json.loads(client.request(auth_url, post=loginData, headers={"Content-Type": "application/json;charset=UTF-8", "Content-Length": len(loginData)}))
+        loginHeaders = {"Content-Type": "application/json;charset=UTF-8", "Content-Length": len(loginData)}
+        if headers:
+            loginHeaders.update(headers)
+        authAnswer = json.loads(client.request(auth_url, post=loginData, headers=loginHeaders))
         if authAnswer["success"]:
             jwtToken = authAnswer["token"]
-            res = json.loads(client.request(userinfo_url, cookie="jwt=%s" % jwtToken))
+            res = json.loads(client.request(userinfo_url, cookie="jwt=%s" % jwtToken, headers=headers))
             if res["success"]:
                 control.setSetting("hasPremium", str(res["isPremium"]))
     except:
@@ -282,8 +542,8 @@ def login():
 def getJWTToken():
     if control.setting("email") and control.setting("password"):
         jwtToken =  control.setting("jwtToken")
-        if jwtToken:
-            m = re.search('(.*)\.(.*)\.(.*)', base64.b64decode(jwtToken).decode("utf-8"))
+        if len(jwtToken) > 0:
+            m = re.search(r'(.*)\.(.*)\.(.*)', base64.b64decode(jwtToken).decode("utf-8"))
             if m:
                 errMsg = ""
                 try:
@@ -304,7 +564,7 @@ def getJWTToken():
                 except:
                     xbmc.log(errMsg, xbmc.LOGERROR)
             else:
-                xbmc.log("TV2 Play: jwtToken not match to (.*)\.(.*)\.(.*). jwtToken: %s" % jwtToken, xbmc.LOGERROR)
+                xbmc.log("TV2 Play: jwtToken not match to (.*)\\.(.*)\\.(.*). jwtToken: %s" % jwtToken, xbmc.LOGERROR)
         jwtToken = login()
         if jwtToken:
             control.setSetting("loggedIn", "true")
